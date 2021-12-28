363441b9a48d525cac435d177194140107ff7ce0
Cindy <cindydunham4@gmail.com>
2021-12-27 19:07:47 -0500

    question buttons

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const startButton = document.getElementById('startbtn')
+const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionsElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -7,8 +11,29 @@ function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionsElement.innerText = question.question
+}
+
+function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'Commonly used data types DO NOT include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. booleans', correct: false },
+            { text: '3. alerts', correct: true },
+            { text: '4. numbers', correct: false }
+        ]
+    }
+]
